cf2838f2f1cea9c7e74fef9f94a0691c75b588ef
Linashihova <linashikhova@mail.ru.>
2026-05-21 09:02:38 +0500

    Добавьте файлы проекта.

---
+++ Models/Messages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents.Models
+{
+    public class Messages
+    {
+        /// <summary>
+        /// Код сообщения
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Отправитель
+        /// </summary>
+        public int UserFrom { get; set; }
+
+        /// <summary>
+        /// Получатель
+        /// </summary>
+        public int UserTo { get; set; }
+
+        /// <summary>
+        /// Текст сообщения
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Конструктор для сообщения
+        /// </summary>
+        public Messages(int userFrom, int userTo, string message)
+        {
+            UserFrom = userFrom;
+            UserTo = userTo;
+            Message = message;
+        }
+
+        /// <summary>
+        /// Пустой конструктор для Entity Framework
+        /// </summary>
+        public Messages() { }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents.Models
+{
+    public class Users
+    {
+        /// <summary>
+        /// Код пользователя
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string Lastname { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Firstname { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Фотография (хранится в виде массива байтов)
+        /// </summary>
+        public byte[] Photo { get; set; }
+
+        /// <summary>
+        /// Конструктор для заполнения объекта
+        /// </summary>
+        public Users(string lastname, string firstname, string surname, byte[] photo)
+        {
+            Lastname = lastname;
+            Firstname = firstname;
+            Surname = surname;
+            Photo = photo;
+        }
+
+        /// <summary>
+        /// Пустой конструктор (нужен для Entity Framework)
+        /// </summary>
+        public Users() { }
+
+        /// <summary>
+        /// Получить ФИО пользователя
+        /// </summary>
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
