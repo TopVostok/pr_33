86732cf3f49df72657589159ef50ef307d46f1ab
Linashihova <linashikhova@mail.ru.>
2026-05-21 09:14:57 +0500

    Разработанные контексты данных Peshin

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatStudents.Classes.Common;
+using ChatStudents.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ChatStudents.Classes
+{
+    public class MessagesContext
+    {
+        public DbSet<Messages> Massages { get; set; }
+        public MessagesContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        // Говорим что используем SQL Server со следующей конфигурацией
+        optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatStudents.Classes.Common;
+using ChatStudents.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public DbSet<Messages> Messages { get; set; }
+
+        public UsersContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
